d0767eac0e0ac5d4c9b2dd995c6c37b06aab4e82
John Weachock <jweachock@gmail.com>
2018-02-19 12:41:14 -0500

    Testing command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+  // https://mattgathu.github.io/writing-cli-app-rust/
+  let matches = App::new("milk")
+    .version("0.0.1")
+    .author("John Weachock <jweachock@gmail.com>")
+    .about("An experiment in Rust and Git.")
+    .arg(Arg::with_name("url")
+         .required(true)
+         .takes_value(true)
+         .index(1)
+         .help("urrlrl"))
+    .get_matches();
+  let url = matches.value_of("url").unwrap();
+  println!("{}", url);
 }
