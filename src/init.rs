05c9bb4304eb50f56bbecf83b6438b91e57ad3a1
John Weachock <jweachock@gmail.com>
2018-02-19 13:18:46 -0500

    Add some rough scaffolding for subcommands
    Thanks to matthiasbeyer/imag

---
+++ src/init.rs
@@ -0,0 +1,19 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
+fn main() {
+  // https://mattgathu.github.io/writing-cli-app-rust/
+  let matches = App::new("milk-init")
+    .version("0.0.1")
+    .author("John Weachock <jweachock@gmail.com>")
+    .about("An experiment in Rust and Git.")
+    .arg(Arg::with_name("url")
+         .required(true)
+         .takes_value(true)
+         .index(1)
+         .help("urrlrl"))
+    .get_matches();
+  let url = matches.value_of("url").unwrap();
+  println!("milk-init {}", url);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate clap;
 
-use clap::{Arg, App};
+use clap::{Arg, App, AppSettings};
+use std::io::ErrorKind;
+use std::process::Command;
+use std::process::Stdio;
+use std::process::exit;
 
 fn main() {
   // https://mattgathu.github.io/writing-cli-app-rust/
@@ -8,12 +12,54 @@ fn main() {
     .version("0.0.1")
     .author("John Weachock <jweachock@gmail.com>")
     .about("An experiment in Rust and Git.")
-    .arg(Arg::with_name("url")
-         .required(true)
-         .takes_value(true)
-         .index(1)
-         .help("urrlrl"))
+    .setting(AppSettings::AllowExternalSubcommands)
     .get_matches();
-  let url = matches.value_of("url").unwrap();
-  println!("{}", url);
+
+  // https://github.com/matthiasbeyer/imag/blob/4dca497c75c5795e948a4e5c523c2ae6c905fcf8/bin/src/main.rs
+  match matches.subcommand() {
+    (subcommand, Some(scmd)) => {
+      let subcommand_args : Vec<&str> = match scmd.values_of("") {
+        Some(values) => values.collect(),
+        None => Vec::new(),
+      };
+
+      let cmd = Command::new(format!("milk-{}", subcommand))
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .args(&subcommand_args[..])
+        .spawn()
+        .and_then(|mut handle| handle.wait());
+
+      match cmd {
+        Ok(exit_status) => {
+          if !exit_status.success() {
+            println!("{} exited with non-zero exit code", subcommand);
+            exit(exit_status.code().unwrap_or(1));
+          }
+        },
+
+        Err(e) => {
+          match e.kind() {
+            ErrorKind::NotFound => {
+              println!("No such command: 'milk-{}'", subcommand);
+              exit(2);
+            },
+
+            ErrorKind::PermissionDenied => {
+              println!("No permission to execute 'milk-{}'", subcommand);
+              exit(1);
+            },
+
+            _ => {
+              println!("Error executing 'milk-{}': {:?}", subcommand, e);
+              exit(1);
+            },
+          }
+        },
+      }
+    },
+
+    _ => {},
+  }
 }
